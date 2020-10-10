25b998045574e4b8f4eea55007ce6a4db9e7b69a
Bayardo Pineda <bayardo2294@gmail.com>
2020-10-10 14:03:32 -0500

    El sistema ahora permite la creacion de mensajes

--- src/main/java/com/mycompany/mensajes_app/Inicio.java
+++ src/main/java/com/mycompany/mensajes_app/Inicio.java
@@ -6,6 +6,7 @@
 package com.mycompany.mensajes_app;
 
 import java.sql.Connection;
+import java.util.Scanner;
 
 /**
  *
@@ -15,12 +16,46 @@ public class Inicio {
     
     public static void main(String []arg ){
         Conexion conexion = new Conexion();
+        Connection cnx=conexion.get_connection();
         
-        try(Connection cnx=conexion.get_connection())
-        {
-        }catch(Exception e){
-            System.out.println(e);
-        }
+        
+        
+        
+        Scanner sc = new Scanner(System.in);
+        int opcion=0;
+        
+        do{
+            System.out.println("-----------------------------");
+            System.out.println("Aplicacion de mensajes");
+            System.out.println("1. crear mensaje");
+            System.out.println("2. listar mensajes");
+            System.out.println("3. Editar mensaje");
+            System.out.println("4. Eliminar mensaje");
+            System.out.println("5. Salir");
+            
+            opcion =sc.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    MensajeService.crearMensajeDB(cnx);
+                case 2:
+                    MensajeService.listarMensajes();
+                case 3:
+                    MensajeService.borrarMensajeDB();
+                case 4:
+                    MensajeService.actualizarMensajeDB();
+                default:
+                    break;
+            
+            }
+            
+            
+        }while(opcion!=5); 
+        
+        
+        
+        
+        
         
     }
     
